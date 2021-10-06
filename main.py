286b9c63424287d9ee3ef0f1959b173b54c7481d
Theo Beech <theobeech@ymail.com>
2021-10-06 09:40:47 +0100

    Changed to 3x3 matrix

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import matrix
 
-class matrix2:
-    def __init__(self,a11,a12,a21,a22):
+class matrix3:
+    def __init__(self,a11,a12,a13,a21,a22,a23,a31,a32,a33):
         self.a11 = a11
         self.a12 = a12
+        self.a13 = a13
         self.a21 = a21
         self.a22 = a22
+        self.a23 = a23
+        self.a31 = a31
+        self.a32 = a32
+        self.a33 = a33
     def form(self):
-        matrix.twobytwo(self.a11,self.a12,self.a21,self.a22)
+        matrix.threebythree(self.a11,self.a12,self.a13,self.a21,self.a22,self.a23,self.a31,self.a32,self.a33)
 
-a = matrix2(1,6,9,48)
+a = matrix3(1,6,9,29,34,48,98,63,40)
 
 a.form()
 
--- matrix.py
+++ matrix.py
@@ -4,3 +4,7 @@ def twobytwo(a11,a12,a21,a22):
     matrixa = np.array([[a11,a12],[a21,a22]])
     return print(matrixa)
 
+def threebythree(a11,a12,a13,a21,a22,a23,a31,a32,a33):
+    matrixa = np.array([[a11,a12,a13],[a21,a22,a23],[a31,a32,a33]])
+    return print(matrixa)
+
