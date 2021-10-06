cbdb0b46ec4502f9df0d358a55d22a9121480d64
Theo Beech <theobeech@ymail.com>
2021-10-06 09:23:48 +0100

    Commit to main - initial solution

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import matrix
 
+class matrix2:
+    def __init__(self,a11,a12,a21,a22):
+        self.a11 = a11
+        self.a12 = a12
+        self.a21 = a21
+        self.a22 = a22
+    def form(self):
+        matrix.twobytwo(self.a11,self.a12,self.a21,self.a22)
+
+a = matrix2(1,6,9,48)
+
+a.form()
+
--- matrix.py
+++ matrix.py
@@ -1,1 +1,6 @@
 import numpy as np 
+
+def twobytwo(a11,a12,a21,a22):
+    matrixa = np.array([[a11,a12],[a21,a22]])
+    return print(matrixa)
+
